128c61d2ce6af1a7b57e5e7a81abc125a1db1d79
Alan Casal <alan.casal.dev@gmail.com>
2025-04-13 15:52:09 -0300

    Implement authentication redirection and update layout for sign-in, sign-up, and reset password screens

--- client/app/(auth)/_layout.tsx
+++ client/app/(auth)/_layout.tsx
@@ -1,12 +1,29 @@
-import { Stack } from 'expo-router';
+import { useAuth } from '@clerk/clerk-expo';
+import { Redirect, Stack } from 'expo-router';
 
-export default function AuthRoutesLayout() {
+const AuthRoutesLayout = () => {
+	const { isSignedIn, isLoaded } = useAuth();
+
+	if (!isLoaded) return null;
+
+	if (isSignedIn) return <Redirect href='/(index)' />;
+
 	return (
 		<Stack>
 			<Stack.Screen
 				name='index'
-				options={{ headerShown: false }}
+				options={{ headerTitle: 'Sign In' }}
+			/>
+			<Stack.Screen
+				name='signup'
+				options={{ headerTitle: 'Sign Up' }}
+			/>
+			<Stack.Screen
+				name='reset-password'
+				options={{ headerTitle: 'Reset Password' }}
 			/>
 		</Stack>
 	);
-}
+};
+
+export default AuthRoutesLayout;
