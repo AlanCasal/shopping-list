297ac53440826d85dfa79cc2f5d9335a95d39b56
Alan Casal <alan.casal.dev@gmail.com>
2025-04-13 17:09:56 -0300

    added template sign-in logic & sign-up link

--- client/app/(auth)/index.tsx
+++ client/app/(auth)/index.tsx
@@ -1,10 +1,21 @@
+import { useSignIn } from '@clerk/clerk-expo';
 import { ThemedText } from '@components/ThemedText';
+import { Link, useRouter } from 'expo-router';
+import { useState } from 'react';
 import { View } from 'react-native';
 
 const SignInScreen = () => {
+	const { signIn, setActive, isLoaded } = useSignIn();
+	const router = useRouter();
+
+	const [emailAddress, setEmailAddress] = useState('');
+	const [password, setPassword] = useState('');
+	const [isSigningIn, setIsSigningIn] = useState(true);
+
 	return (
 		<View>
-			<ThemedText type='title'>SignInScreen</ThemedText>
+			<ThemedText type='title'>SignIn Screen</ThemedText>
+			<Link href='/sign-up'>Go to Sign Up</Link>
 		</View>
 	);
 };
